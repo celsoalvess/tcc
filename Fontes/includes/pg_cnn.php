c4197f761cf8dcb79b4c61980f9c2631e5939970
Celso Alves <34461959+celsoalvess@users.noreply.github.com>
2018-11-26 18:42:44 -0200

    correção cnx

--- Fontes/includes/pg_cnn.php
+++ Fontes/includes/pg_cnn.php
@@ -1,5 +1,5 @@
 <?php
 	require_once("db/adodb5/adodb.inc.php");
 	$c = ADONewConnection("postgres");
-	$c->PConnect("host=127.0.0.1 port=5432 dbname=tcc user=postgres password=klamidia666") or die ("Erro na conexão!!");	
+	$c->PConnect("host=127.0.0.1 port=5432 dbname=tcc user=postgres password=senha123") or die ("Erro na conexão!!");	
 ?>
